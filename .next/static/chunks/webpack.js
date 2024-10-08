7fd829f0e23f8e1e903cfdc0e4b1d722e291dd89
Denis <dh92fr@gmail.com>
2024-10-09 00:16:40 +0300

    netlify

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -170,7 +170,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb2b4458183a73ae"; }
+/******/ 		__webpack_require__.h = function() { return "2e2111e16206adfd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
